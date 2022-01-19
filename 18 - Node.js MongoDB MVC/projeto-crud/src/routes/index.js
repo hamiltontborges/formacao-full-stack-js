2e940f17e054935cfb92a57ae6198ea351d84d23
Hamilton Tumenas Borges <65918255+hamiltontborges@users.noreply.github.com>
2022-01-19 15:05:21 -0300

    inclusão de rotas, model e controller de customers

---
+++ 18 - Node.js MongoDB MVC/projeto-crud/src/routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+
+const CustomersController = require('../controllers/customers')
+
+//rotas
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Título teste'
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Cadastro de Clientes'
+    })
+})
+
+router.post('/register/add', CustomersController.add)
+
+module.exports = router
