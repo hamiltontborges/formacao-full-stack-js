dda265f863ff63d1a85f978ea571b81b61abd99c
Hamilton Tumenas Borges <hamilton.borges@sou.unifeob.edu.br>
2022-01-22 13:43:21 -0300

    react-counter - inclusao do usestate e useeffect

---
+++ 20 - React.js Básico/react-counter/src/Counter.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react"
+import './Counter.css'
+
+
+const Counter = () => {
+
+    const [contador, setContador] = useState(0)
+
+    useEffect(() => {
+        console.log('renderizou')
+    }, [contador])
+
+
+    const handleOnClick = operator => {
+        const novoValor = operator === '+'
+            ? contador + 1
+            : contador - 1
+
+        setContador(novoValor)
+    }
+
+    return (
+        <div className="counter">
+            <span>{contador}</span>
+            <button onClick={() => handleOnClick('-')}>-</button>
+            <button onClick={() => handleOnClick('+')}>+</button>
+        </div>
+    )
+}
+
+export default Counter
