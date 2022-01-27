0eef50e4fc2a6fb9b9b8169788b0a16606a91f54
Hamilton Tumenas Borges <hamilton.borges@sou.unifeob.edu.br>
2022-01-27 16:39:19 -0300

    nextjs - rotas dinamicas

---
+++ 23 - Next.js/hello-world-next/pages/[category]/[...products].js
@@ -0,0 +1,17 @@
+import { route } from 'next/dist/server/router'
+import { useRouter } from 'next/router'
+
+const Products = () => {
+    const router = useRouter()
+
+    console.log(router)
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>{router.query.category} / {router.query.products}</p>
+        </div>
+    )
+}
+
+export default Products
